954b3aa4c0425a626602b79da9c8b2272f48b846
Kzu <alvaro.vega.pavez@gmail.com>
2025-01-08 07:36:19 -0300

    add: repository DoctorRepository

---
+++ src/main/java/com/alertasmedicas/app/doctor/repository/DoctorRepository.java
@@ -0,0 +1,9 @@
+package com.alertasmedicas.app.doctor.repository;
+
+import com.alertasmedicas.app.doctor.entity.Doctor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoctorRepository extends CrudRepository<Doctor, Long> {
+}
